802a24a6996c133c6833e6f244e1570cf655afba
tunganh252 <tunganh2521999@gmail.com>
2020-10-04 10:44:26 +0700

    -- First commit -- [Implement] source

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var question = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal',
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    ));
+  }
+}
