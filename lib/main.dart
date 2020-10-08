b434083eee16413e118390beb2c81323ac396e09
tunganh252 <tunganh2521999@gmail.com>
2020-10-09 00:20:34 +0700

    [Update] -- tenary expression + custom data maping

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './question.dart';
 import './answer.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,53 +15,64 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var question = [
+  final question = const [
     {
       'textQuestion': 'What\'s your favorite color ',
-      'answers': ['Red', 'Green', 'Blue', 'Yellow']
+      'answers': [
+        {'text': 'Red', 'score': 11},
+        {'text': 'Green', 'score': 13},
+        {'text': 'Blue', 'score': 12},
+        {'text': 'Yellow', 'score': 14},
+      ]
     },
     {
       'textQuestion': 'What\'s your favorite animal',
-      'answers': ['Cat', 'Dog', 'Chicken']
+      'answers': [
+        {'text': 'Cat', 'score': 5},
+        {'text': 'Dog', 'score': 4},
+        {'text': 'Chicken', 'score': 8},
+      ]
     },
     {
       'textQuestion': 'What\'s your favorite food ',
-      'answers': ['Sandwich', 'Hamburger', 'Pizza']
+      'answers': [
+        {'text': 'Sandwich', 'score': 23},
+        {'text': 'Hamburger', 'score': 32},
+        {'text': 'Chicken', 'score': 27},
+      ],
     },
   ];
 
   var _indexQuestion = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
-    if (_indexQuestion >= question.length - 1) {
-      setState(() {
-        _indexQuestion = 0;
-      });
-      return;
-    }
-    print('length arr: ${question.length}');
-    print('_indexQuestion: ${_indexQuestion}');
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _indexQuestion = _indexQuestion + 1;
     });
+    print(_totalScore);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("My First App"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Question(question[_indexQuestion]['textQuestion']),
-          ...(question[_indexQuestion]['answers'] as List<String>)
-              .map((answer) {
-            return Answer(answer, _answerQuestion);
-          })
-        ],
-      ),
-    ));
+            appBar: AppBar(
+              title: Text("My First App"),
+            ),
+            body: _indexQuestion < question.length
+                ? Column(
+                    children: <Widget>[
+                      Question(question[_indexQuestion]['textQuestion']),
+                      ...(question[_indexQuestion]['answers']
+                              as List<Map<String, Object>>)
+                          .map((answer) {
+                        return Answer(answer['text'],
+                            () => _answerQuestion(answer['score']));
+                      })
+                    ],
+                  )
+                : Result()));
   }
 }
